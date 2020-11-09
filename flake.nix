a5774a63898eb9bf4b2064c9e5bd79789972175c
PedroRegisPOAR <pedroalencarregis@hotmail.com>
2020-11-08 23:20:49 -0300

    Still trying to make worw 6

--- flake.nix
+++ flake.nix
@@ -1,30 +1,15 @@
 {
-
-  description = "Reproducible development environment with reasonable defaults";	
- 
-  inputs = rec {
-    # stable.url = "github:NixOS/nixpkgs/nixos-20.03";
-    unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    # home-manager = {
-    #  url = "github:rycee/home-manager";
-    #  inputs.nixpkgs.follows = "stable";
-    # };
-
-    # nur.url = "github:nix-community/NUR";
-    # emacs.url = "github:nix-community/emacs-overlay";
-    nixos-hardware.url = "github:NixOS/nixos-hardware";
-
-    nix-direnv = {
-      url = "github:nix-community/nix-direnv";
-      flake = false;
-    };
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs-channels/nixos-unstable;
+    # nur.url = github:nix-community/NUR;
   };
 
-  outputs = { self, nixpkgs }: {
-     # replace 'pedro' with your hostname here.
-     nixosConfigurations.pedro = nixpkgs.lib.nixosSystem {
-       system = "x86_64-linux";
-       modules = [ ./configuration.nix ];
-     };
+  outputs = { nixpkgs, nix, self, ... }@inputs: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules =
+        [ (import ./configuration.nix) ];
+      specialArgs = { inherit inputs; };
+    };
   };
 }
