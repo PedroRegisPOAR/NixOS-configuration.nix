89c4589855b5724437ce67466ed5137e62af3829
PedroRegisPOAR <pedroalencarregis@hotmail.com>
2020-11-15 02:10:30 -0300

    Tropin flake.nix

--- flake.nix
+++ flake.nix
@@ -1,29 +1,21 @@
 {
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-20.09";
-    nixos-hardware.url = "github:NixOS/nixos-hardware";
-    nixos-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-    home-manager = {
-      url = "github:nix-community/home-manager/release-20.09";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-  };
+  description = "Reproducible dev environment flakes";
 
-  outputs = { self, nixpkgs, nixos-hardware, nixos-unstable, home-manager, ... }@inputs: {
-    nixosConfigurations.fedora = nixpkgs.lib.nixosSystem rec {
-      system = "x86_64-linux";
-      modules = [
-        (nixpkgs.nixosModules.notDetected)
-        (nixos-hardware.nixosModules.lenovo-thinkpad-t430)
-        (let
-          overlay-unstable = final: prev: {
-            unstable = nixos-unstable.legacyPackages.${system};
-          };
-         in { nixpkgs.overlays = [ overlay-unstable ]; })
-        (./configuration.nix)
-      ];
-      specialArgs = { inherit inputs; };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-20.03";
+    nixos-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+  outputs = { self, nixpkgs, ... }@inputs: {
+
+    nixosConfigurations = {
+      xenia = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ (import ./configuration.nix) ];
+        specialArgs = { inherit inputs; };
+      };
     };
+
+    xenia = self.nixosConfigurations.xenia.config.system.build.toplevel;
+
   };
 }
-
