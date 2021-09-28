6255a09d4420911af83e5e34a7e2fc229ecd70d6
PedroRegisPOAR <pedroalencarregis@hotmail.com>
2021-09-28 14:24:04 -0300

    Sad ugly workaround, readOnlyStore = false;

--- configuration.nix
+++ configuration.nix
@@ -25,10 +25,13 @@
      experimental-features = nix-command flakes ca-references ca-derivations
    '';
 
-  # From:
-  # https://github.com/sherubthakur/dotfiles/blob/be96fe7c74df706a8b1b925ca4e7748cab703697/system/configuration.nix#L44
-  # pointing to: https://github.com/NixOS/nixpkgs/issues/124215
-  sandboxPaths = [ "/bin/sh=${pkgs.bash}/bin/sh"];
+    # From:
+    # https://github.com/sherubthakur/dotfiles/blob/be96fe7c74df706a8b1b925ca4e7748cab703697/system/configuration.nix#L44
+    # pointing to: https://github.com/NixOS/nixpkgs/issues/124215
+    sandboxPaths = [ "/bin/sh=${pkgs.bash}/bin/sh"];
+    
+    # https://github.com/NixOS/nixpkgs/blob/fd8a7fd07da0f3fc0e27575891f45c2f88e5dd44/nixos/modules/services/misc/nix-daemon.nix#L323
+    readOnlyStore = false;
   };
 
    
