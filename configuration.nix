1becf308e77cab7eba7911bae451d397d1260be8
PedroRegisPOAR <pedroalencarregis@hotmail.com>
2022-07-05 22:26:59 -0300

    Changes readOnlyStore from false to  true

--- configuration.nix
+++ configuration.nix
@@ -45,7 +45,9 @@
     
     # https://github.com/NixOS/nixpkgs/blob/fd8a7fd07da0f3fc0e27575891f45c2f88e5dd44/nixos/modules/services/misc/nix-daemon.nix#L323
     # Be carefull if using it as false!
-    readOnlyStore = false;
+    # Ohh crap, around 20/06/2022 I ran something like sudo rm -fr $TMPDIR/* and I destroyed 
+    # my system completely because this flag was forced to be false!  
+    readOnlyStore = true;
   };
 
    
