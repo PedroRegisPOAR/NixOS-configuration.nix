c56d019718e9b19d3c91f853ec78e83125c4fd65
Pedro O. A. Regis <pedroalencarregis@hotmail.com>
2022-07-08 00:13:43 -0300

    Removes extraLocaleSettings and changes defaultLocale

--- configuration.nix
+++ configuration.nix
@@ -61,19 +61,19 @@
   time.timeZone = "America/Recife";
 
   # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.utf8";
+  i18n.defaultLocale = "en_US.UTF-8";
 
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "pt_BR.utf8";
-    LC_IDENTIFICATION = "pt_BR.utf8";
-    LC_MEASUREMENT = "pt_BR.utf8";
-    LC_MONETARY = "pt_BR.utf8";
-    LC_NAME = "pt_BR.utf8";
-    LC_NUMERIC = "pt_BR.utf8";
-    LC_PAPER = "pt_BR.utf8";
-    LC_TELEPHONE = "pt_BR.utf8";
-    LC_TIME = "pt_BR.utf8";
-  };
+  # i18n.extraLocaleSettings = {
+  #  LC_ADDRESS = "pt_BR.utf8";
+  #  LC_IDENTIFICATION = "pt_BR.utf8";
+  #  LC_MEASUREMENT = "pt_BR.utf8";
+  #  LC_MONETARY = "pt_BR.utf8";
+  #  LC_NAME = "pt_BR.utf8";
+  #  LC_NUMERIC = "pt_BR.utf8";
+  #  LC_PAPER = "pt_BR.utf8";
+  #  LC_TELEPHONE = "pt_BR.utf8";
+  #  LC_TIME = "pt_BR.utf8";
+  #};
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
