96906d18bc4b86b27ee945248160109293dd7ccd
PedroRegisPOAR <pedroalencarregis@hotmail.com>
2022-07-31 01:03:11 -0300

    WIP, testing locale stuff

--- configuration.nix
+++ configuration.nix
@@ -80,17 +80,17 @@
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
 
-  # i18n.extraLocaleSettings = {
+  i18n.extraLocaleSettings = {
   #  LC_ADDRESS = "pt_BR.utf8";
   #  LC_IDENTIFICATION = "pt_BR.utf8";
   #  LC_MEASUREMENT = "pt_BR.utf8";
-  #  LC_MONETARY = "pt_BR.utf8";
+    LC_MONETARY = "pt_BR.utf8";
   #  LC_NAME = "pt_BR.utf8";
   #  LC_NUMERIC = "pt_BR.utf8";
   #  LC_PAPER = "pt_BR.utf8";
   #  LC_TELEPHONE = "pt_BR.utf8";
   #  LC_TIME = "pt_BR.utf8";
-  #};
+  };
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
