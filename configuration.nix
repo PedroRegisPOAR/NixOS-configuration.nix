6a4bd9cf88448f1872e9e01b06baa2a7a4886b6d
Pedro <pedroalencarregis@hotmail.com>
2023-02-28 01:13:23 -0300

    Adds localConf

--- configuration.nix
+++ configuration.nix
@@ -300,6 +300,38 @@
   # https://github.com/NixOS/nixpkgs/issues/176081#issuecomment-1145825623
   fonts = {
     fontDir.enable = true;
+      fontconfig = {
+        localConf = ''
+        <?xml version="1.0"?>
+        <!DOCTYPE fontconfig SYSTEM "fonts.dtd">
+        <fontconfig>
+          <alias binding="weak">
+            <family>monospace</family>
+            <prefer>
+              <family>emoji</family>
+            </prefer>
+          </alias>
+          <alias binding="weak">
+            <family>sans-serif</family>
+            <prefer>
+              <family>emoji</family>
+            </prefer>
+          </alias>
+          <alias binding="weak">
+            <family>serif</family>
+            <prefer>
+              <family>emoji</family>
+            </prefer>
+          </alias>
+        </fontconfig>
+      '';
+      defaultFonts = {
+        emoji = [ "Noto Color Emoji" ];
+        monospace = [ "FreeMono" ];
+        sansSerif = [ "FreeSans" ];
+        serif = [ "FreeSerif" ];
+      };
+    };
     fonts = with pkgs; [
        # fontconfig
 
